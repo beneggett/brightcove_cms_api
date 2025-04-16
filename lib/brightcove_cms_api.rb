41f317d86cb5b0dde0ec4abe7722372faa31284d
Evan Shabsove <evan.shabsove@gmail.com>
2025-04-16 18:43:32 -0400

    I am very bad at working with gems and forget that things dont just get auto required

--- lib/brightcove_cms_api.rb
+++ lib/brightcove_cms_api.rb
@@ -1,3 +1,4 @@
 require_relative "brightcove_cms_api/version"
 require_relative "brightcove_cms_api/video"
+require_relative "brightcove_cms_api/ingest"
 require_relative "brightcove_cms_api/errors"
